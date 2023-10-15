8b7191880eab3154de3e7601e63549a77518066e
Morteza Damghani <mortezadamghani77@gmail.com>
2023-10-14 22:51:20 -0400

    all_empty_points_finder is implemented

--- my_player3.py
+++ my_player3.py
@@ -47,6 +47,47 @@ def input_file_reader():
     return input_my_stone_color, input_previous_board, input_current_board
 
 
+# This function finds all of the empty squares around the stones of the input color
+def all_of_my_empty_neighbors_finder(input_my_stone_color, input_current_board):
+    output_list = []
+    board_size = len(input_current_board[0])
+    i = 0
+    while i < board_size:
+        j = 0
+        while j < board_size:
+            if input_current_board[i][j] == input_my_stone_color:
+                if (i + 1) < board_size and input_current_board[i + 1][j] == 0:
+                    output_list.append([i + 1, j])
+
+                if (i - 1) >= 0 and input_current_board[i - 1][j] == 0:
+                    output_list.append([i - 1, j])
+
+                if (j + 1) < board_size and input_current_board[i][j + 1] == 0:
+                    output_list.append([i, j + 1])
+
+                if (j - 1) >= 0 and input_current_board[i][j - 1] == 0:
+                    output_list.append([i, j - 1])
+
+            j += 1
+        i += 1
+
+    return output_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Go game is implemented in this function
 def go_game(input_my_stone_color, input_previous_board, input_current_board):
     board_size = len(input_current_board[0])
@@ -71,8 +112,12 @@ def go_game(input_my_stone_color, input_previous_board, input_current_board):
         if all_zero:
             return [2, 2]
 
-        else:
-            return "all are not zero"
+        all_of_my_empty_neighbors = all_of_my_empty_neighbors_finder(input_my_stone_color, input_current_board)
+
+
+
+
+
 
 
 
