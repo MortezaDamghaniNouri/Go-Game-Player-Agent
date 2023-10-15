19548a2062d3b726307fa03ebae167dbeb3d9760
Morteza Damghani <mortezadamghani77@gmail.com>
2023-10-14 21:08:38 -0400

    input_file_reader function is implemented

--- my_player3.py
+++ my_player3.py
@@ -1,26 +1,43 @@
 
+def input_file_reader():
+    lines = []
+    input_file = open("input.txt", "rt")
+    while True:
+        line = input_file.readline()
+        if line == "":
+            break
+        lines.append(line.rstrip("\n"))
+    input_file.close()
+    input_my_stone_color = int(lines[0])
+    input_previous_board = []
+    input_current_board = []
+    i = 1
+    while i <= 5:
+        temp = list(lines[i])
+        new_temp = []
+        j = 0
+        while j < len(temp):
+            new_temp.append(int(temp[j]))
+            j += 1
+        input_previous_board.append(new_temp)
+        i += 1
+
+    i = 6
+    while i < len(lines):
+        temp = list(lines[i])
+        new_temp = []
+        j = 0
+        while j < len(temp):
+            new_temp.append(int(temp[j]))
+            j += 1
+        input_current_board.append(new_temp)
+        i += 1
+
+    return input_my_stone_color, input_previous_board, input_current_board
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# The main part of the code starts here
+my_stone_color, previous_boar, current_board = input_file_reader()
 
 
 
